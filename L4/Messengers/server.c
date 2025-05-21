a9d0baea41b68a2f20e758a1e0dd58a59df9bbec
felikshetalia <mccorsica@yahoo.com>
2025-05-21 19:51:03 +0200

    server: stage 4

--- L4/Messengers/server.c
+++ L4/Messengers/server.c
@@ -6,6 +6,8 @@
 #define MAX_MESSAGE_SIZE 64
 #define BACKLOG 3
 
+volatile int do_work = 1;
+
 const char* greek_cities[20] = {
     "Athens",
     "Sparta",
@@ -48,6 +50,10 @@ typedef struct {
     int fd;
 } Client_t;
 
+void sigint_handler(int sig){
+    do_work = 0;
+}
+
 void add_new_client_handler(struct epoll_event ev, Client_t* ClientList){
 
 }
@@ -65,18 +71,34 @@ void give_count(int * city_ownership){
 
 }
 
-void broadcast_message(int cli, char* msg, int active_connections, Client_t* list){
+void remove_client(int epfd, int active_connections, Client_t* list, int clientfd){
+    epoll_ctl(epfd, EPOLL_CTL_DEL, clientfd, NULL);
+    close(clientfd);
+    for(int i = 0; i < active_connections; i++){
+        if(list[i].fd == clientfd){
+            for (int j = i; j < active_connections - 1; j++){
+                list[j] = list[j+1];
+            }
+            active_connections--;
+            break;
+        }
+    }
+}
+
+void broadcast_message(int cli, char* msg, int active_connections, Client_t* list, int epfd){
     for(int i = 0; i < active_connections; i++){
         if(cli != list[i].fd){
             if(write(list[i].fd, msg, strlen(msg)+1) < 0){
                 if(errno != EPIPE)
                     ERR("write");
+                remove_client(epfd, active_connections, list, list[i].fd);
             }
         }
     }
 }
 
-void handle_client_messages(int accepted_client, int* city_ownership, int active_connections, Client_t* list){
+
+void handle_client_messages(int epfd, int accepted_client, int* city_ownership, int active_connections, Client_t* list){
     int ret;
  
     char input[4];
@@ -84,7 +106,9 @@ void handle_client_messages(int accepted_client, int* city_ownership, int active
     if(ret < 0)
         ERR("read");
     if(ret == 0){
-        close(accepted_client);
+        printf("[%d] Client disconnected\n", accepted_client);
+        remove_client(epfd, active_connections,list, accepted_client);
+        //close(accepted_client);
         return;
     }
     // if(ret == 4){
@@ -98,20 +122,18 @@ void handle_client_messages(int accepted_client, int* city_ownership, int active
         int city_id;
         int tens = input[1] - '0';
         int ones = input[2] - '0';
-        if(tens>0){
-            city_id = tens*10 + ones;
+        city_id = tens*10 + ones;
+        if(city_id >= 1 || city_id <= 20){
+            if(input[0] == 'p')
+                city_ownership[city_id-1] = 1;
+            else
+                city_ownership[city_id-1] = 0;
+    
+            broadcast_message(accepted_client, input, active_connections, list, epfd);
+    
+            // make it persian
         }
-        else{
-            city_id = ones;
-        }
-        if(input[0] == 'p')
-            city_ownership[city_id-1] = 1;
-        else
-            city_ownership[city_id-1] = 0;
 
-        broadcast_message(accepted_client, input, active_connections, list);
-
-        // make it persian
     }
     give_count(city_ownership);
 }
@@ -124,7 +146,7 @@ int main(int argc, char** argv){
     if(port < 1024){
         usage(argv[0]);
     }
-
+    sethandler(sigint_handler, SIGINT);
     int server_fd = bind_tcp_socket(port, BACKLOG);
     int new_flags = fcntl(server_fd, F_GETFL) | O_NONBLOCK;
     fcntl(server_fd, F_SETFL, new_flags);
@@ -149,7 +171,7 @@ int main(int argc, char** argv){
     Client_t ClientList[MAX_CLIENTS];
     int city_ownership[20] = {0};
     // 0- greek; 1- persian
-    while(1){
+    while(do_work){
         if((nfds = epoll_wait(epoll_descriptor, events, MAX_EVENTS, -1)) > 0){
             for(int i = 0; i < nfds; i++){
                 if(events[i].data.fd == server_fd){
@@ -186,7 +208,7 @@ int main(int argc, char** argv){
                 }
                 else{
                     // get messages from connected clients
-                    handle_client_messages(events[i].data.fd, city_ownership, active_connections, ClientList);
+                    handle_client_messages(epoll_descriptor, events[i].data.fd, city_ownership, active_connections, ClientList);
                 }
             }
         }
