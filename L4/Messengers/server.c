2db8c66c8b1625809e73258308f7abf849905531
felikshetalia <mccorsica@yahoo.com>
2025-05-21 17:37:29 +0200

    server: stage 2

--- L4/Messengers/server.c
+++ L4/Messengers/server.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 #define MAX_CLIENTS 4
+#define MAX_EVENTS (MAX_CLIENTS) + 1
 #define MAX_USERNAME_LENGTH 32
 #define MAX_MESSAGE_SIZE 64
 #define BACKLOG 3
@@ -43,6 +44,33 @@ void usage(char* program_name)
     exit(EXIT_FAILURE);
 }
 
+typedef struct {
+    int fd;
+} Client_t;
+
+void add_new_client_handler(struct epoll_event ev, Client_t* ClientList){
+
+}
+
+void handle_client_messages(int accepted_client){
+    char input[4];
+    int ret;
+    do{
+        ret = bulk_read(accepted_client, input, 4);
+        if(ret < 0)
+            ERR("read");
+        if(ret == 0){
+            if(TEMP_FAILURE_RETRY(close(accepted_client)) < 0)
+                ERR("close");
+    
+        }
+        if(ret > 0){
+            printf("%s\n", input);
+        }
+
+    } while(ret <= sizeof(char[4]));
+}
+
 int main(int argc, char** argv){
     if(argc != 2){
         usage(argv[0]);
@@ -53,27 +81,74 @@ int main(int argc, char** argv){
     }
 
     int server_fd = bind_tcp_socket(port, BACKLOG);
+    int new_flags = fcntl(server_fd, F_GETFL) | O_NONBLOCK;
+    fcntl(server_fd, F_SETFL, new_flags);
+    // set up epoll
+    int epoll_descriptor;
+    if ((epoll_descriptor = epoll_create1(0)) < 0)
+    {
+        ERR("epoll_create:");
+    }
+    struct epoll_event event, events[MAX_EVENTS];
+    event.events = EPOLLIN;
+    event.data.fd = server_fd;
+    if (epoll_ctl(epoll_descriptor, EPOLL_CTL_ADD, server_fd, &event) == -1)
+    {
+        perror("epoll_ctl: listen_sock");
+        exit(EXIT_FAILURE);
+    }
 
-    char input[4];
     // 1st stage is a single client. no epoll needed
-
-    int accepted_client = add_new_client(server_fd);
-
-        int ret = bulk_read(accepted_client, input, 4);
-        if(ret < 0)
-            ERR("read");
-        if(ret == 0){
-            if(TEMP_FAILURE_RETRY(close(accepted_client)) < 0)
-                ERR("close");
-
+    int active_connections = 0;
+    int nfds;
+    Client_t ClientList[MAX_CLIENTS];
+    while(1){
+        if((nfds = epoll_wait(epoll_descriptor, events, MAX_EVENTS, -1)) > 0){
+            for(int i = 0; i < nfds; i++){
+                if(events[i].data.fd == server_fd){
+                    if(active_connections >= MAX_CLIENTS){
+                        // we reject
+                        if (write(events[i].data.fd, "Server is full\n", 16) < 0)
+                        {
+                            if (errno != EPIPE)
+                            {
+                                ERR("write");
+                            }
+                            if (TEMP_FAILURE_RETRY(close(events[i].data.fd)) < 0)
+                                ERR("close");
+                        }
+                    }
+                    else{
+                        // add a NEW client
+                        int accepted_client = add_new_client(events[i].data.fd);
+                        ClientList[active_connections].fd = accepted_client;
+                        printf("[%d] New client connected\n",ClientList[active_connections].fd);
+                        //set up a new epoll event for client messages
+                        struct epoll_event client_event;
+                        client_event.events = EPOLLIN;
+                        client_event.data.fd = ClientList[active_connections].fd;
+                        if (epoll_ctl(epoll_descriptor, EPOLL_CTL_ADD, ClientList[active_connections].fd, &client_event) == -1)
+                        {
+                            perror("epoll_ctl: listen_sock");
+                            exit(EXIT_FAILURE);
+                        }
+                        active_connections++;
+                    }
+                    
+                }
+                else{
+                    // get messages from connected clients
+                    handle_client_messages(events[i].data.fd);
+                }
+            }
         }
-        if(ret > 0){
-            printf("%s\n", input);
-        }
+
+    }
 
     
     if(TEMP_FAILURE_RETRY(close(server_fd)) < 0)
         ERR("close");
-
+    if(TEMP_FAILURE_RETRY(close(epoll_descriptor)) < 0)
+        ERR("close");
     return EXIT_SUCCESS;
 }
