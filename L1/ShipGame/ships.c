de406811bfbbd181bee9ff3f5a3c52db26dc46a6
felikshetalia <mccorsica@yahoo.com>
2025-09-03 17:01:30 +0200

    Initial commit

---
+++ L1/ShipGame/ships.c
@@ -0,0 +1,32 @@
+#define _POSIX_C_SOURCE 200809L
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/mman.h>
+
+#define ERR(source) \
+    (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+void usage(char *name){
+    fprintf(stderr, "USAGE: %s n\n", name);
+    fprintf(stderr, "4<=n<=7 \n m>=4\n n*m <= 52");
+    exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char **argv){
+    usage(argv[0]);
+    return 0;
+}
