eff3d528f3acc559e4f556b18ba1e7b124bd1785
felikshetalia <mccorsica@yahoo.com>
2025-09-21 11:01:35 +0200

    stage 1

---
+++ L3/Roncevaux/sop-knights.c
@@ -0,0 +1,114 @@
+#define _POSIX_C_SOURCE 200809L
+#include <dirent.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <time.h>
+#include <unistd.h>
+
+#define ERR(source) \
+    (fprintf(stderr, "%s:%d\n", __FILE__, __LINE__), perror(source), kill(0, SIGKILL), exit(EXIT_FAILURE))
+
+#define MAX_KNIGHT_NAME_LENGTH 20
+
+int set_handler(void (*f)(int), int sig)
+{
+    struct sigaction act = {0};
+    act.sa_handler = f;
+    if (sigaction(sig, &act, NULL) == -1)
+        return -1;
+    return 0;
+}
+
+void msleep(int millisec)
+{
+    struct timespec tt;
+    tt.tv_sec = millisec / 1000;
+    tt.tv_nsec = (millisec % 1000) * 1000000;
+    while (nanosleep(&tt, &tt) == -1)
+    {
+    }
+}
+
+typedef struct{
+    char name[MAX_KNIGHT_NAME_LENGTH];
+    int HP;
+    int attack;
+} knight_t;
+
+int count_descriptors()
+{
+    int count = 0;
+    DIR* dir;
+    struct dirent* entry;
+    struct stat stats;
+    if ((dir = opendir("/proc/self/fd")) == NULL)
+        ERR("opendir");
+    char path[PATH_MAX];
+    getcwd(path, PATH_MAX);
+    chdir("/proc/self/fd");
+    do
+    {
+        errno = 0;
+        if ((entry = readdir(dir)) != NULL)
+        {
+            if (lstat(entry->d_name, &stats))
+                ERR("lstat");
+            if (!S_ISDIR(stats.st_mode))
+                count++;
+        }
+    } while (entry != NULL);
+    if (chdir(path))
+        ERR("chdir");
+    if (closedir(dir))
+        ERR("closedir");
+    return count - 1;  // one descriptor for open directory
+}
+
+int main(int argc, char* argv[])
+{
+    srand(getpid());
+
+    FILE* franciFile, *saraceniFile;
+    if((franciFile = fopen("franci.txt", "r")) == NULL){
+        printf("Franks have not arrived on the battlefield\n");
+        ERR("fopen");
+    }
+    if((saraceniFile = fopen("saraceni.txt", "r")) == NULL){
+        printf("Saracens have not arrived on the battlefield\n");
+        ERR("fopen");
+    }
+
+    int franciNo, saraceniNo;
+    // EOF exists
+    fscanf(franciFile, "%d", &franciNo);
+    fscanf(saraceniFile, "%d", &saraceniNo);
+
+    knight_t* FranciSoldiers, *SaraceniSoldiers;
+
+    if((FranciSoldiers = calloc(franciNo, sizeof(knight_t))) == NULL)
+        ERR("calloc");
+    if((SaraceniSoldiers = calloc(saraceniNo, sizeof(knight_t))) == NULL)
+        ERR("calloc");
+
+    for(int i = 0; i < franciNo; i++){
+        fscanf(franciFile, "%s %d %d", FranciSoldiers[i].name, &FranciSoldiers[i].HP, &FranciSoldiers[i].attack);
+        printf("I am Frankish knight %s I will serve my king with my %d HP and %d attack\n", FranciSoldiers[i].name, FranciSoldiers[i].HP, FranciSoldiers[i].attack);
+    }
+    for(int i = 0; i < saraceniNo; i++){
+        fscanf(saraceniFile, "%s %d %d", SaraceniSoldiers[i].name, &SaraceniSoldiers[i].HP, &SaraceniSoldiers[i].attack);
+        printf("I am Saraceni knight %s I will serve my king with my %d HP and %d attack\n", SaraceniSoldiers[i].name, SaraceniSoldiers[i].HP, SaraceniSoldiers[i].attack);
+    }
+    fclose(franciFile);
+    fclose(saraceniFile);
+    free(FranciSoldiers);
+    free(SaraceniSoldiers);
+    printf("Opened descriptors: %d\n", count_descriptors());
+    return 0;
+}
