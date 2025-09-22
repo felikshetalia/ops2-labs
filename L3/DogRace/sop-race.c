fb876e87eecb8dd5a71c31e38469a7d301d6bf24
felikshetalia <mccorsica@yahoo.com>
2025-09-22 22:01:25 +0200

    stage 1 barriers done

--- L3/DogRace/sop-race.c
+++ L3/DogRace/sop-race.c
@@ -40,19 +40,6 @@ void usage(char* program_name)
     exit(EXIT_FAILURE);
 }
 
-typedef struct
-{
-    // pthread_barrier_t barr[MAX_DOG_COUNT];
-    pthread_barrier_t barr;
-
-    pthread_mutex_t runMutex;
-} SharedZone_t;
-
-// typedef struct
-// {
-//     int dogPid;
-// } TrackSegment_t;
-
 void msleep(unsigned int milli)
 {
     time_t sec = (int)(milli / 1000);
@@ -64,139 +51,58 @@ void msleep(unsigned int milli)
         ERR("nanosleep");
 }
 
-pthread_mutexattr_t init_mtx()
-{
-    pthread_mutexattr_t mutexattr;
-    pthread_mutexattr_init(&mutexattr);
-    pthread_mutexattr_setpshared(&mutexattr, PTHREAD_PROCESS_SHARED);
-    pthread_mutexattr_setrobust(&mutexattr, PTHREAD_MUTEX_ROBUST);
-    return mutexattr;
-}
+typedef struct {
+    pthread_barrier_t barrier;
+} DogTools_t;
 
-void mutex_lock_handler(pthread_mutex_t* mutex)
-{
-    int error;
-    if ((error = pthread_mutex_lock(mutex)) != 0)
-    {
-        if (error == EOWNERDEAD)
-        {
-            pthread_mutex_consistent(mutex);
-        }
-        else
-        {
-            ERR("pthread_mutex_lock");
-        }
-    }
-}
-
-void dog_work(SharedZone_t* sharedDt, int* track, int L)
-{
-    pthread_barrier_wait(&sharedDt->barr);
-    printf("Dog [%d] waiting...\n", getpid());
+void dog_work(DogTools_t* tool){
+    pthread_barrier_wait(&tool->barrier);
+    printf("[%d] waf waf waf (started race)\n", getpid());
     sleep(1);
-
-    for (int i = 0; i < L; i++)
-    {
-        track[i] = 0;
-    }
-    // each dog will move here
-    while (1)
-    {
-        msleep(rand() % MAX_SLEEP + MIN_SLEEP);
-        int move = rand() % MAX_MOVEMENT + MIN_MOVEMENT;
-        int dogPid = getpid();
-        int curridx = 0;
-        curridx += move;
-        mutex_lock_handler(&sharedDt->runMutex);
-        if (curridx <= MAX_TRACK_LEN)
-        {
-            if (track[curridx] > 0)
-            {
-                printf("[%d] waf waf (occupied)", getpid());
-            }
-            track[curridx] = dogPid;
-        }
-
-        pthread_mutex_unlock(&sharedDt->runMutex);
-
-        printf("[%d] Dog is at pos [%d]", getpid(), curridx);
-    }
 }
 
-void birth_dogs(int dogsNo, SharedZone_t* sharedDt, int* track, int L)
-{
+void dog_breed(int N, DogTools_t* tool){
     srand(getpid());
     pid_t pid;
-    for (int i = 0; i < dogsNo; i++)
-    {
+    for(int i=0;i<N;i++){
         pid = fork();
-        if (pid == 0)
-        {
-            dog_work(sharedDt, track, L);
-            printf("[%d] waf waf (started race)\n", getpid());
-            munmap(sharedDt, sizeof(SharedZone_t));
-            // munmap(track, sizeof(TrackSegment_t) * racetrackLen);
-            munmap(track, sizeof(int) * L);
-
+        if(pid == 0){
+            dog_work(tool);
+            munmap(tool, sizeof(DogTools_t));
             exit(0);
         }
-        if (pid < 0)
-        {
+        if(pid < 0){
             ERR("fork");
         }
+
     }
 }
+
 
 int main(int argc, char** argv)
 {
-    if (argc != 3)
+    if(argc != 3)
         usage(argv[0]);
-    int racetrackLen = atoi(argv[1]);
-    int dogsNo = atoi(argv[2]);
-    if (racetrackLen < 16 || racetrackLen > 256 || dogsNo < 2 || dogsNo > 6)
-    {
+
+    int L, N;
+    L = atoi(argv[1]);
+    N = atoi(argv[2]);
+
+    if(L < MIN_TRACK_LEN || L > MAX_TRACK_LEN || N < MIN_DOG_COUNT || N > MAX_DOG_COUNT)
         usage(argv[0]);
-    }
 
-    SharedZone_t* sharedDt;
-    if ((sharedDt = (SharedZone_t*)mmap(NULL, sizeof(SharedZone_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,
-                                        -1, 0)) == MAP_FAILED)
-    {
+    DogTools_t* tool;
+    if(MAP_FAILED == (tool = (DogTools_t*)mmap(NULL, sizeof(DogTools_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0)))
         ERR("mmap");
-    }
-    int* track;
-    if ((track = (int*)mmap(NULL, sizeof(int) * racetrackLen, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1,
-                            0)) == MAP_FAILED)
-    {
-        ERR("mmap");
-    }
 
-    // pthread_mutexattr_t mutexattr = init_mtx();
-    // pthread_mutex_init(&sharedDt->runMutex, &mutexattr);
     pthread_barrierattr_t attr;
-    pthread_barrierattr_setpshared(&attr, PTHREAD_PROCESS_SHARED);
-    // for (int i = 0; i < MAX_DOG_COUNT; i++)
-    //     pthread_barrier_init(&sharedDt->barr[i], &attr, dogsNo);
-
-    pthread_barrier_init(&sharedDt->barr, &attr, dogsNo);
-
-    birth_dogs(dogsNo, sharedDt, track, racetrackLen);
-    while (wait(NULL) > 0)
-        ;
-
-    // if(msync(sharedDt, sizeof(SharedZone_t), MS_SYNC)<0){
-    //     ERR("msync");
-    // }
-    // for (int i = 0; i < MAX_DOG_COUNT; i++)
-    //     pthread_barrier_destroy(&sharedDt->barr[i]);
-
-    pthread_barrier_destroy(&sharedDt->barr);
+    pthread_barrierattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
+    pthread_barrier_init(&tool->barrier, &attr, 1);
+    dog_breed(N, tool);
+    while(wait(NULL) > 0);
+    pthread_barrier_destroy(&tool->barrier);
     pthread_barrierattr_destroy(&attr);
-    // pthread_mutex_destroy(&sharedDt->runMutex);
-    // pthread_mutexattr_destroy(&mutexattr);
-    munmap(sharedDt, sizeof(SharedZone_t));
-    // munmap(sharedDt, sizeof(pid_t) - 1);
-    munmap(track, sizeof(int) * racetrackLen);
+    munmap(tool, sizeof(DogTools_t));
     return 0;
 }
 
